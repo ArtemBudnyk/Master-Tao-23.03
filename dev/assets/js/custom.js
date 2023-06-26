6c733168336519849115497af5171b1ab9b81cdb
ArtemBudnyk <qrtleo@gmail.com>
2023-06-26 17:48:24 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,13 @@
-  
+   $(document).ready(function(){
+      $('.first-screen-slider').slick({
+        dots: true,
+        appendArrows:'.first-screen-slider__arrows'
+      });
+});
+
+// $(document).ready(function() {
+//     $('.slider').slick();
+//     $('.slider').slick('slick-dots', {
+//       container: '.slider-dots',
+//     });
+//   });
