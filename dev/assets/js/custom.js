21000959acee08c224e799a3c814479fb213da79
ArtemBudnyk <qrtleo@gmail.com>
2023-07-13 10:55:13 +0300

    advantages slider donr

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -25,3 +25,34 @@ document.getElementById('myfile').onchange = function () {
   document.getElementById('file-name').innerHTML = this.value;
 }
 
+
+// advantage
+
+
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
